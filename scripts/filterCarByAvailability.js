dc2687df0e99cde5554340861b449d27711e6cf1
blackjac7 <febriansyah705@gmail.com>
2024-03-07 11:49:56 +0700

    modify js files

--- scripts/filterCarByAvailability.js
+++ scripts/filterCarByAvailability.js
@@ -1,12 +1,49 @@
 function filterCarByAvailability(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+    // Sangat dianjurkan untuk console.log semua hal hehe
+    console.log(cars);
 
-  // Tempat penampungan hasil
-  const result = [];
+    // Tempat penampungan hasil
+    const result = [];
 
-  // Tulis code-mu disini
+    // Tulis code-mu disini
+    cars.forEach((car) => {
+        // ? Check availability
+        if (car.available) {
+            // TODO: Push car with true available
+            result.push(car);
+        }
+    });
 
-  // Rubah code ini dengan array hasil filter berdasarkan availablity
-  return result;
+    // for (let i = 0; i < cars.length; i++) {
+    //     // Check availability
+    //     if (cars[i].available) {
+    //         result.push(cars[i]);
+    //     }
+    // }
+
+    // for (let i in cars) {
+    //     // Check availability
+    //     if (cars[i].available) {
+    //         result.push(cars[i]);
+    //     }
+    // }
+
+    // for (let car of cars) {
+    //     // Check availability
+    //     if (car.available) {
+    //         result.push(car);
+    //     }
+    // }
+
+    // let i = 0;
+    // while (i < cars.length) {
+    //     // Check availability
+    //     if (cars[i].available) {
+    //         result.push(cars[i]);
+    //     }
+    //     i++;
+    // }
+
+    // Rubah code ini dengan array hasil filter berdasarkan availablity
+    return result;
 }
