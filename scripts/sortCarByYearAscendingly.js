611fa505c6a5b51160ab113f14b3a4d227045794
blackjac7 <febriansyah705@gmail.com>
2024-03-08 17:35:34 +0700

    modify js files

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -7,7 +7,21 @@ function sortCarByYearAscendingly(cars) {
     const result = [...cars];
 
     // Tulis code-mu disini
-    result.sort((a, b) => a.year - b.year);
+    // result.sort((a, b) => a.year - b.year);
+
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < result.length - 1; i++) {
+            if (result[i].year > result[i + 1].year) {
+                // Swap elements
+                let temp = result[i];
+                result[i] = result[i + 1];
+                result[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
 
     // Rubah code ini dengan array hasil sorting secara ascending
     return result;
--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -7,7 +7,21 @@ function sortCarByYearDescendingly(cars) {
     const result = [...cars];
 
     // Tulis code-mu disini
-    result.sort((a, b) => b.year - a.year);
+    // result.sort((a, b) => b.year - a.year);
+
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < result.length - 1; i++) {
+            if (result[i].year < result[i + 1].year) {
+                // Swap elements
+                let temp = result[i];
+                result[i] = result[i + 1];
+                result[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
 
     // Rubah code ini dengan array hasil sorting secara descending
     return result;
